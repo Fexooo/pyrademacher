651b52d565372edba4807281b7939288241f04c1
Felix Katzenberg <felix.katzenberg@fh-dortmund.de>
2022-10-18 21:14:14 +0200

    Changed package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="homepilot",
+    name="customHomepilot",
     version="0.6.8",
     author="Pedro Ribeiro",
     author_email="pedroeusebio@gmail.com",
