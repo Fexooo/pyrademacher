74b624c987456c94a67cb54d315ed242dc3294b1
Felix <39031231+Fexooo@users.noreply.github.com>
2022-10-18 20:19:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyrademacher",
+    name="homepilot",
     version="0.6.7",
     author="Pedro Ribeiro",
     author_email="pedroeusebio@gmail.com",
